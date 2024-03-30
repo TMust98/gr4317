c50c073e0edf6010e160df19892f6bf8fa738a4b
unknown <timmust98@gmail.com>
2024-03-30 12:39:25 +0300

    flask-5

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Имя пользователя', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField('Пароль', validators=[DataRequired(), EqualTo('password', 'Пароли не совпадают')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Ник занят')
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from app import app
-from flask import render_template, url_for, redirect, flash, send_from_directory, request
-from app.forms import LoginForm
+from flask import render_template, url_for, redirect, flash, send_from_directory, request, abort
+from app.forms import LoginForm, RegisterForm
 from app.models import User
 import os
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from app import db
 
 
 @app.route('/favicon.ico', methods=['GET','POST'])
@@ -43,9 +44,28 @@ def login():
     return render_template('login.html', form=form, title="Вход")
 
 
+@app.route('/regiser', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        flash('Вы уже зарегистрированы! Выйдите из системы для регистрации.', 'warning')
+        return redirect(url_for('index'))
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_passwrod(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Успешная регистрация. Ваш ID {user.id}', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form, title='Регистрация')
+
+
 @app.route('/lk')
-@login_required
+#@login_required
 def lk():
+    if current_user.is_anonymous:
+        abort(403)
     return render_template('lk.html', title='ЛК')
 
 @app.route('/logout')
